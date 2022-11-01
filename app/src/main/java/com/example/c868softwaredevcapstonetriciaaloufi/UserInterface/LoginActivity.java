aea03bfbffdaa8deda29b1eb8bc622b6f97ce471
evilqueen599 <triciaalhammadi@gmail.com>
2022-11-01 18:52:06 -0400

    theme corrected

--- app/src/main/java/com/example/c868softwaredevcapstonetriciaaloufi/UserInterface/LoginActivity.java
+++ app/src/main/java/com/example/c868softwaredevcapstonetriciaaloufi/UserInterface/LoginActivity.java
@@ -1,15 +1,24 @@
 package com.example.c868softwaredevcapstonetriciaaloufi.UserInterface;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.c868softwaredevcapstonetriciaaloufi.R;
 
 public class LoginActivity extends AppCompatActivity {
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login_activity);
+    }
+
+    public void registerANewUser(View view) {
+        Intent intent = new Intent(LoginActivity.this, RegisterActivity.class);
+        startActivity(intent);
     }
 }
---
+++ app/src/main/java/com/example/c868softwaredevcapstonetriciaaloufi/UserInterface/RegisterActivity.java
@@ -0,0 +1,15 @@
+package com.example.c868softwaredevcapstonetriciaaloufi.UserInterface;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.c868softwaredevcapstonetriciaaloufi.R;
+
+public class RegisterActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.register_activity);
+    }
+}

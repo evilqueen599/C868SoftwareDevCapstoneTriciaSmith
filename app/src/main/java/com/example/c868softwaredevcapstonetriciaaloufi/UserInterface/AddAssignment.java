ad38719e3173dd6463319577cb2ebbdcede81a27
evilqueen599 <triciaalhammadi@gmail.com>
2022-11-02 16:12:20 -0400

    all layouts added

--- app/src/main/java/com/example/c868softwaredevcapstonetriciaaloufi/UserInterface/AddAssignment.java
+++ app/src/main/java/com/example/c868softwaredevcapstonetriciaaloufi/UserInterface/AddAssignment.java
@@ -1,6 +1,16 @@
 package com.example.c868softwaredevcapstonetriciaaloufi.UserInterface;
 
+import android.os.Bundle;
+
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.example.c868softwaredevcapstonetriciaaloufi.R;
+
 public class AddAssignment extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.add_assignment_activity);
+    }
 }

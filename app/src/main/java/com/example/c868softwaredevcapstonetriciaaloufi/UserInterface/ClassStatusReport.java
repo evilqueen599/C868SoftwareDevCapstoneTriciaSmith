79f782ec76e2f6c7968be117b22f7156dd75a15f
evilqueen599 <triciaalhammadi@gmail.com>
2022-11-06 14:08:31 -0500

    report now does not show until a selection is made!

--- app/src/main/java/com/example/c868softwaredevcapstonetriciaaloufi/UserInterface/ClassStatusReport.java
+++ app/src/main/java/com/example/c868softwaredevcapstonetriciaaloufi/UserInterface/ClassStatusReport.java
@@ -1,28 +1,20 @@
 package com.example.c868softwaredevcapstonetriciaaloufi.UserInterface;
 
 import android.os.Bundle;
-import android.view.KeyEvent;
-import android.view.MotionEvent;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
-import android.widget.TableLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.recyclerview.widget.GridLayoutManager;
-import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 import androidx.recyclerview.widget.StaggeredGridLayoutManager;
 
-import com.example.c868softwaredevcapstonetriciaaloufi.Adapters.ClassAdapter;
 import com.example.c868softwaredevcapstonetriciaaloufi.Adapters.ClassReportAdapter;
-import com.example.c868softwaredevcapstonetriciaaloufi.Adapters.SemesterAdapter;
 import com.example.c868softwaredevcapstonetriciaaloufi.Database.Repository;
 import com.example.c868softwaredevcapstonetriciaaloufi.Models.Classes;
-import com.example.c868softwaredevcapstonetriciaaloufi.Models.Semesters;
 import com.example.c868softwaredevcapstonetriciaaloufi.R;
 
 import java.sql.Timestamp;
@@ -67,7 +59,6 @@ public class ClassStatusReport extends AppCompatActivity {
         ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.SpinnerAdapter, android.R.layout.simple_spinner_item);
         adapter.setDropDownViewResource(androidx.appcompat.R.layout.support_simple_spinner_dropdown_item);
         classStatusSpinner.setAdapter(adapter);
-        classAdapter.setClasses(classesFull);
        int currentSelection = classStatusSpinner.getSelectedItemPosition();
        classStatusSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
            @Override
